21be8445134263e32f5d23936cf72205ea639558
SnappySM <nicola.scognamillo@estudiantat.upc.edu>
2024-11-14 10:31:40 +0100

    La llibreria de Google es carrega en iniciar el component.

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -8,9 +8,17 @@ import {Component, OnInit} from '@angular/core';
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  private scriptLoaded = false;
 
   ngOnInit(): void {
     (window as any).handleOauthResponse = this.handleOauthResponse.bind(this);
+    this.loadGoogleScript()
+      .then(() => {
+        this.scriptLoaded = true;
+      })
+      .catch((error) => {
+        console.error('Error loading Google script:', error);
+      });
   }
 
   decodeJWTToken(token: string): any {
@@ -21,5 +29,23 @@ export class LoginComponent {
     const responsePayload = this.decodeJWTToken(response.credential);
     console.log(responsePayload);
     sessionStorage.setItem('loggedinUser', JSON.stringify(responsePayload));
+  }
+
+  loadGoogleScript(): Promise<void> {
+    return new Promise((resolve, reject) => {
+      if (this.scriptLoaded) {
+        resolve();
+        return;
+      }
+      const script = document.createElement('script');
+      script.src = 'https://accounts.google.com/gsi/client';
+      script.async = true;
+      script.onload = () => {
+        this.scriptLoaded = true;
+        resolve();
+      };
+      script.onerror = (error) => reject(error);
+      document.body.appendChild(script);
+    });
   }
 }
